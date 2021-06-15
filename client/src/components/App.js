8fe4fb8d50e2e081f9fc2e47cd063bd689d27820
janevsky <baze.janevski@gmail.com>
2021-06-15 19:55:20 +0200

    Added all branches

---
+++ client/src/components/App.js
@@ -0,0 +1,51 @@
+// styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/styles/loginForm.css';
+import '../assets/styles/routeHeader.css'
+
+
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import Nav from "../components/widgets/navmenu/nav"
+import Error from "../components/ui/Messages/error"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Create from './pages/RecipeCreate';
+import Dashboard from "./ui/Dashboard"
+import Header from "./ui/header"
+
+
+const App = () => {
+  return (
+      <div className="App">
+          <Router>
+              <div>
+                  <Header />
+                  <Switch>
+                      <Route exact path="/">
+                          <Home />
+                      </Route>
+                      <Route path="/login">
+                          <Login />
+                      </Route>
+                      <Route path="/register">
+                          <Register />
+                      </Route>
+                      <Route path="/create">
+                          <Create />
+                      </Route>
+                      <Route path="/dashboard">
+                          <Dashboard />
+                      </Route>
+                      <Route path="*">
+                          <Error />
+                      </Route>
+                  </Switch>
+              </div>
+          </Router>
+      </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,11 @@
+//import { Form, Container, Col, Row, Button } from 'react-bootstrap';
+
+const Home = (props) => {
+    return (
+        <div className='home'>
+            <p>Home page {props.title}</p>
+        </div>
+    )
+}
+
+export default Home;
